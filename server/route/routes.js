a14c8d0fcfcb38a9ea030377140eab64b1c1c953
Elisey Sinkevich <selisej@yandex.ru>
2019-09-13 11:48:37 +0300

    control step templates, jobs: api and services

--- server/route/routes.js
+++ server/route/routes.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const titulController = require('../controller/titulController.js');
-const upload = require('../controller/upload');
-const dictionaryController = require('../controller/dictionaryController')
-const contractController = require('../controller/contractController')
+const uploadController = require('../controller/uploadController');
+const dictionaryController = require('../controller/dictionaryController');
+const contractController = require('../controller/contractController');
+const controlStepController = require('../controller/controlStepController');
 
 router.get('/titul/:id', titulController.getTitulById);
 router.get('/tituls', titulController.getTituls);
 
-router.post('/upload', upload);
+router.post('/upload', uploadController.upload);
 
 router.get('/departments', dictionaryController.getDepartments);
 router.get('/documentKinds', dictionaryController.getDocumentKinds);
@@ -27,6 +28,9 @@ router.get('/contracts', contractController.getContracts);
 router.get('/acts', contractController.getActs);
 router.get('/orders', contractController.getOrders);
 router.post('/act', contractController.postAct);
+
+router.get('/controlStepTemplates', controlStepController.getControlStepTemplates);
+router.get('/controlStepJobs', controlStepController.getControlStepJobs);
 
 module.exports = router;
 
---
+++ src/app/models/control-step-job-model.ts
@@ -0,0 +1,18 @@
+export interface ControlStepJobModel {
+  id: number;
+  number: number;   // Номер пункта
+  templateId: number;   // Шаблон контрольных этапов
+  type: string;                 // Вид этапа
+  isHead: boolean;              // Признак "Головной этап"
+  name: string;                 // Наименование
+  minenergoName: string;        // Наименование этапов Минэнерго
+  planStartDate: string;        // Планируемая дата начала
+  planEndDate: string;          // Планируемая дата окончания
+  factStartDate: string;        // Фактическая дата начала
+  factEndDate: string;          // Фактическая дата окончания
+  planCost: string;             // Планируемая стоимость без НДС
+  factCost: string;             // Фактическая стоимость без НДС
+  completionPercent: string;    // Процент выполнения
+  documentConnection: string;    // Типы документов для связи
+}
+
