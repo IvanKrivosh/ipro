1bb481138556342072c34a033fc908de99a9599b
Elisey Sinkevich <selisej@yandex.ru>
2019-09-13 13:44:17 +0300

    getting jobs by templateId

--- src/app/services/control-step-service.ts
+++ src/app/services/control-step-service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpEventType, HttpParams, HttpRequest, HttpResponse} from '
 import {Observable, Subject} from 'rxjs';
 import {ControlStepTemplateModel} from '../models/control-step-template-model';
 import {ControlStepJobModel} from '../models/control-step-job-model';
+import {TitulInfo} from '../models/titul-model';
 
 const api = '/api';
 
@@ -16,8 +17,8 @@ export class ControlStepService {
   }
 
   public getControlStepJobs(controlStepTemplateId: number): Observable<Array<ControlStepJobModel>> {
-    const httpParams = new HttpParams();
-    httpParams.append('templateId', controlStepTemplateId.toString());
+    let httpParams = new HttpParams();
+    httpParams = httpParams.append('templateId', controlStepTemplateId.toString());
     return this.http.get<Array<ControlStepJobModel>>(`${api}/controlStepJobs`, {params: httpParams});
   }
 }
