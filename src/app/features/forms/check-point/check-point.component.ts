0c1c59ce21f3d3dd1473af69fda9bed856f61d69
Andrei Naumov <adnaumov@users.noreply.github.com>
2019-09-14 22:48:03 +0300

    Update check-point.component.ts

--- src/app/features/forms/check-point/check-point.component.ts
+++ src/app/features/forms/check-point/check-point.component.ts
@@ -61,7 +61,3 @@ export class CheckPointComponent implements OnInit {
     });
   }
 }
-
-function randomDate(start, end) {
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-}
